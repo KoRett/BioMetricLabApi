60a8a438e58e767588d4b171d6aaffd9ad1a2ac0
KoRett <kashencevrostik@gmail.com>
2025-03-16 15:07:45 +0300

    Initial commit

---
+++ src/main/java/ru/korett/biometriclab/BioMetricLabApplication.java
@@ -0,0 +1,13 @@
+package ru.korett.biometriclab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioMetricLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioMetricLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/korett/biometriclab/BioMetricLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.korett.biometriclab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioMetricLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
